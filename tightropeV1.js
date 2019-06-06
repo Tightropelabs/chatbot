76e77a138671b1bd2c4b36606ac6c592c66e2186
hillerc <chillbasslines@gmail.com>
2019-06-06 11:30:09 -0700

    take 2

--- tightropeV1.js
+++ tightropeV1.js
@@ -71,6 +71,13 @@ function init(config) {
       if (postId !== brokerageId) {
         config.userId = preId + brokerageId;
         configure(config)
+        sendEvent({
+          type: 'set-brokerage',
+          channel: 'web',
+          payload: {
+            brokerage: brokerageId
+          }
+        })
       }
     }
   })
